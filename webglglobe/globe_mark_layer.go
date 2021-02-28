6eb7633872a9535a37061fc493d4f899084c47c0
go4orward <4orward@gmail.com>
2021-02-28 16:17:06 +0900

    globe experimental codes added

---
+++ webglglobe/globe_mark_layer.go
@@ -0,0 +1,31 @@
+package webglglobe
+
+import (
+	"github.com/go4orward/gowebgl/webgl2d"
+)
+
+type GlobeMarkLayer struct {
+	ScnObjs []*webgl2d.SceneObject // 2D SceneObjects to be rendered (in CAMERA space)
+}
+
+func NewGlobeMarkLayer() *GlobeMarkLayer {
+	mlayer := GlobeMarkLayer{}
+	mlayer.ScnObjs = make([]*webgl2d.SceneObject, 0)
+	return &mlayer
+}
+
+// ----------------------------------------------------------------------------
+// Mark 						(single instance with its own geometry)
+// ----------------------------------------------------------------------------
+
+func (self *GlobeMarkLayer) AddMark(geometry *webgl2d.Geometry, color string) *GlobeMarkLayer {
+	return self
+}
+
+// ----------------------------------------------------------------------------
+// Mark with Instance Poses 	(multiple instances sharing the same geometry)
+// ----------------------------------------------------------------------------
+
+func (self *GlobeMarkLayer) AddMarkWithPoses(geometry *webgl2d.Geometry, poses *webgl2d.SceneObjectPoses) *GlobeMarkLayer {
+	return self
+}
